7defbe81082b2e57b06ec17849bf1c097a70d0b4
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-07-26 23:04:30 +0530

    added button functionality

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const element = document.querySelector("#btnCounter");
 const input = document.querySelector("#inputElement");
 
-// element.addEventListener("click", counterIncrememnt, false),
+let counter = 0;
+
+function counterIncrememnt(){
+   counter = counter + 1;
+   console.log(counter) 
+}
+
+element.addEventListener("click", counterIncrememnt, false)

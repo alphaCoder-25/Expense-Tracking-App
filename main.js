150fa6d8b08d35c03750f8be1dd87af5401be191
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-07-28 20:57:36 +0530

    using array & objects

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const element = document.querySelector("#btnAddExpense");
 const inputElement = document.querySelector("#inputAmount");
+const headingEl = document.querySelector("#headingTotal");
+const inputDescEl = document.querySelector("#inputDesc")
 
 let totalExpense = 0;
 
+headingEl.textContent = totalExpense;
+
+const allExpenses = [];
+
 function addExpenseToTotal() {
-    const textAmount = inputElement.value;
-    console.log({ textAmount });
 
-const expense = parseInt(textAmount, 10)
-console.log({ expense });
+const expenseItem = {};
 
- totalExpense = totalExpense + expense;
- console.log(totalExpense);
+const textAmount = inputElement.value;
+const textDesc = inputDescEl.value;
 
+const expense = parseInt(textAmount, 10);
+
+expenseItem.amount = expense;
+expenseItem.desc = textDesc;
+
+allExpenses.push(expenseItem);
+
+console.clear();
+console.table(allExpenses);
+
+totalExpense = totalExpense + expense;
+headingEl.textContent = totalExpense;
 }
 
-element.addEventListener("click",addExpenseToTotal, false)
+element.addEventListener("click", addExpenseToTotal, false)
